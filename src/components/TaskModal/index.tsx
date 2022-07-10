bf5ccc2b674e34718e77f9ba7e4523f6f2a57189
Allan Scala <49344838+AllanScala1991@users.noreply.github.com>
2022-07-10 11:13:56 -0300

    fix: add readonly and edit btn

--- src/components/TaskModal/index.tsx
+++ src/components/TaskModal/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Button } from "../Button"
 import "./index.css"
 
@@ -17,15 +18,20 @@ interface TaskModalProps {
 }
 
 export function TaskModal(props: TaskModalProps) {
+    const [isReadOnly, setIsReadOnly] = useState(true)
+
     return (
         <div className="modalContainer">
             <div className="modalInfo">
 
                 <div className="inputNameContainer">
                     <label className="labelName">Titulo</label>
-                    <div className="inputNameComponent">
+                    <div className="inputNameComponent" 
+                    style={isReadOnly? {backgroundColor:"#EEEEEE"} : {backgroundColor:"white"}}>
                         <input 
                             type="text" 
+                            style={{backgroundColor:"transparent"}}
+                            readOnly={isReadOnly}
                             className="inputName" 
                             onChange={event => {props.titleOnChange(event.target.value)}}
                             defaultValue={props.title}
@@ -35,8 +41,11 @@ export function TaskModal(props: TaskModalProps) {
 
                 <div className="inputNameContainer text-area">
                     <label className="labelName">Descrição</label>
-                    <div className="inputNameComponent text-area">
+                    <div className="inputNameComponent text-area"
+                    style={isReadOnly? {backgroundColor:"#EEEEEE"} : {backgroundColor:"white"}}>
                         <textarea
+                            style={{backgroundColor:"transparent"}}
+                            readOnly={isReadOnly}
                             onChange={event => {props.descriptionOnChange(event.target.value)}}
                             defaultValue={props.description}
                         ></textarea>
@@ -45,8 +54,13 @@ export function TaskModal(props: TaskModalProps) {
 
                 <div className="inputNameContainer">
                     <label className="labelName">Prioridade</label>
-                    <div className="inputNameComponent">
-                        <select className="inputName" onChange={event => {props.priorityOnChange(event.target.value)}} defaultValue={props.urgency}>
+                    <div className="inputNameComponent"
+                    style={isReadOnly? {backgroundColor:"#EEEEEE"} : {backgroundColor:"white"}}>
+                        <select className="inputName" 
+                        onChange={event => {props.priorityOnChange(event.target.value)}} 
+                        defaultValue={props.urgency}
+                        style={{backgroundColor:"transparent"}}
+                        disabled={isReadOnly}>
                             <option value="low">Baixa</option>
                             <option value="medium">Média</option>
                             <option value="high">Alta</option>
@@ -56,8 +70,13 @@ export function TaskModal(props: TaskModalProps) {
 
                 <div className="inputNameContainer">
                     <label className="labelName">Quadro</label>
-                    <div className="inputNameComponent">
-                        <select className="inputName" onChange={event => {props.currentStageOnChange(event.target.value)}} defaultValue={props.currentStage}>
+                    <div className="inputNameComponent"
+                    style={isReadOnly? {backgroundColor:"#EEEEEE"} : {backgroundColor:"white"}}>
+                        <select className="inputName" 
+                        onChange={event => {props.currentStageOnChange(event.target.value)}} 
+                        defaultValue={props.currentStage}
+                        style={{backgroundColor:"transparent"}}
+                        disabled={isReadOnly}>
                             {
                                 props.stage ?
                                     props.stage.map((stage: any) => {
@@ -72,11 +91,11 @@ export function TaskModal(props: TaskModalProps) {
 
                 <div className="buttonsTaskContainer">
                     <Button 
-                        buttonText="Salvar" 
+                        buttonText={isReadOnly? "Editar" : "Salvar"}
                         buttonTextColor="white"
                         buttonBackground="#1AAE9F"
                         buttonWidth={30}
-                        click= {() => {props.saveModifications()}}
+                        click= {isReadOnly? () => setIsReadOnly(false) : () => {props.saveModifications()}}
                     />
                     <Button 
                         buttonText="Deletar" 
